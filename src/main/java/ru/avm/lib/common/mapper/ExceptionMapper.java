d05b5994730ae4496d8a383e2d255f0285db774f
avm_ <alexeymajor@gmail.com>
2025-11-25 20:16:48 +0300

    refs and fixes

--- src/main/java/ru/avm/lib/common/mapper/ExceptionMapper.java
+++ src/main/java/ru/avm/lib/common/mapper/ExceptionMapper.java
@@ -2,14 +2,6 @@ package ru.avm.lib.common.mapper;
 
 import ru.avm.lib.common.dto.ExceptionDto;
 
-//@Component
-//@Mapper(componentModel = "spring")
 public interface ExceptionMapper {
-
-    //    @Value("${spring.application.name}")
-//    public String applicationName;
-//
-//    @Mapping(target = "source", constant = "java(applicationName)")
-//    @Mapping(target = "exception", expression = "java( ex.getClass().getSimpleName() )")
     ExceptionDto toDto(Throwable ex);
 }
